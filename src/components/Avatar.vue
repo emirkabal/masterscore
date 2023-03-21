173c975dbb499476245379d3f3ec9c8b104ca128
Emir Kabal <kabalemir@gmail.com>
2023-03-21 13:22:50 +0300

    update: better avatar view

--- src/components/Avatar.vue
+++ src/components/Avatar.vue
@@ -3,15 +3,27 @@ const props = defineProps({
   username: {
     type: String,
     required: true
+  },
+  border: {
+    type: Boolean,
+    default: false
   }
 })
 </script>
 <template>
   <div
-    class="flex flex-shrink-0 items-center justify-center overflow-hidden rounded-full border-4 bg-gray-900 dark:border-zinc-800"
+    class="flex flex-shrink-0 items-center justify-center overflow-hidden rounded-full bg-gray-200 dark:bg-gray-800"
+    :class="{
+      'outline outline-4 outline-gray-300 dark:outline-zinc-700': border
+    }"
   >
     <img
-      :src="'https://api.dicebear.com/5.x/thumbs/svg?seed=' + props.username"
+      :src="
+        'https://api.dicebear.com/5.x/thumbs/svg?seed=' +
+        props.username +
+        '&scale=90&backgroundColor=transparent'
+      "
+      class="rounded-full"
       draggable="false"
       alt="User Avatar"
     />
