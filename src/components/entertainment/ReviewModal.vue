2c4530783fb9faf0ab98cdd695b935471107edd1
Emir Kabal <kabalemir@gmail.com>
2023-04-02 06:31:06 +0300

    bugfix: spoilers doesn't work

--- src/components/entertainment/ReviewModal.vue
+++ src/components/entertainment/ReviewModal.vue
@@ -14,7 +14,6 @@ const props = defineProps<{
 
 const modal = ref(false)
 const emojiPicker = ref(null)
-const spoiler = ref(false)
 
 const isEmojiSelector = ref(false)
 onClickOutside(emojiPicker, () => {
@@ -136,14 +135,16 @@ $listen("modal:review", (val) => {
           >
             <Switch
               id="spoiler"
-              v-model="spoiler"
-              :class="spoiler ? 'bg-blue-700' : 'bg-teal-700'"
+              v-model="props.reviewData.spoiler"
+              :class="props.reviewData.spoiler ? 'bg-blue-700' : 'bg-teal-700'"
               class="relative inline-flex h-[18px] w-[32px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
             >
               <span class="sr-only">Use setting</span>
               <span
                 aria-hidden="true"
-                :class="spoiler ? 'translate-x-3.5' : 'translate-x-0'"
+                :class="
+                  props.reviewData.spoiler ? 'translate-x-3.5' : 'translate-x-0'
+                "
                 class="pointer-events-none inline-block h-[14px] w-[14px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out"
               />
             </Switch>
