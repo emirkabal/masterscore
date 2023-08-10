f453915fbed6e87a8b356dcb6cced1b70b2bd555
Emir Kabal <kabalemir@gmail.com>
2023-08-10 04:15:30 +0300

    update version

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@ import { useDark, useToggle, onKeyStroke } from "@vueuse/core"
 const isDark = useDark()
 const toggleDarkMode = useToggle(isDark)
 const version = computed(() => {
-  return "1.0.3-alpha"
+  return "1.0.6-alpha"
 })
 const clickToShow = ref(0)
 const date = useAppConfig().buildDate
